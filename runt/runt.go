701af2026ee336c665eaba7dc26e7c5294281194
Henry Strickland <strick@localhost>
2014-08-21 23:00:00 -0700

    CallV() for PGo.

--- runt/runt.go
+++ runt/runt.go
@@ -1790,6 +1790,22 @@ func (g *PGo) Invoke(field string, aa ...P) P {
 	panic(F("Method on type %q does not exist: %s", g.V.Type(), field))
 }
 
+func (g *PGo) CallV(a1 []P, a2 []P, kv1 []KV, kv2 map[string]P) P {
+  if len(kv1) > 0 || len(kv2) > 0 {
+	  panic("Cannot call GO with named (keyword) parameters.")
+  }
+  if a2 == nil {
+    return g.Call(a1...)
+  }
+  if a1 == nil {
+    return g.Call(a2...)
+  }
+  var aa []P
+  aa = append(aa, a1...)
+  aa = append(aa, a2...)
+  return g.Call(aa...)
+}
+
 func (g *PGo) Call(aa ...P) P {
 	f := MaybeDeref(g.V)
 	if f.Kind() != R.Func {
