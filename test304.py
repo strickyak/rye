cfa1993ac9ba954ecc9e612fbb2fa222614e7e5d
Henry Strickland <strick@localhost>
2014-09-22 23:09:13 -0700

    cant do nexted funcs

--- test304.py
+++ test304.py
@@ -2,3 +2,16 @@ def double(x):
   return x + x
 print double(21)
 print double("beep")
+
+# Nested Functions not supported!
+#def f(x):
+#  def g(y):
+#    return x + y
+#  x = 1000
+#  z = [g]
+#  x = 2000
+#  z.append(g)
+#  return z
+#  
+#for a in f(50):
+#  print a(1)
