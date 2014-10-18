4d6842527b480e9af13fddf493d5ba1df6d5777a
strick <strick@localhost>
2014-10-17 22:16:52 -0700

    __INIT_FIELDS__() calls supers correctly with Tfield.

--- tr.py
+++ tr.py
@@ -1243,13 +1243,16 @@ class CodeGen(object):
               self.glbls[p.name] = ('*pCtor_%s' % (p.name), '&pCtor_%s{PCallSpec: %s}' % (p.name, desc.CallSpec()))
 
       #ctor future
-      print 'func (o *C_%s) __INIT_FIELDS__() {' % p.name
+      print 'func (o *C_%s) RyeInitFields__() {' % p.name
       for iv in self.instvars:
         print '   o.M_%s = None' % iv
-      if p.sup and p.sup.name:
-        print '// superclass:', repr(p.sup.name)
+      if p.sup and type(p.sup) is Tvar:
+        print '// superclass:', p.sup.visit(self)
         if p.sup.name not in ['native', 'object']:
-          print '   o.C_%s.__INIT_FIELDS__()' % p.sup.name
+          print '   o.C_%s.RyeInitFields__()' % p.sup.name
+      if p.sup and type(p.sup) is Tfield:
+        print '// superclass:', p.sup.visit(self)
+        print '   o.C_%s.RyeInitFields__()' % p.sup.field
       print '}'
 
       print ''
