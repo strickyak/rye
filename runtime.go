e292749cded714a15d3fc9e0b23fac532a3da9f0
strick <strick@localhost>
2014-11-13 08:40:35 -0800

    Allow None instead of empty list in len() and for loops (because go thinks empty slices are nil).

--- runtime.go
+++ runtime.go
@@ -674,6 +674,16 @@ func WriteC(w *bytes.Buffer, c rune) {
 	}
 }
 
+// Because go confuses empty lists with nil, rye does the same with None.
+// This saves you writing `for x in vec if vec else []:`
+func (o *PNone) Len() int               { return 0 }
+func (o *PNone) List() []P              { return nil }
+func (o *PNone) Iter() Nexter           {
+	z := &PListIter{PP: nil}
+	z.Self = z
+	return z
+}
+
 func (o *PNone) Bool() bool             { return false }
 func (o *PNone) String() string         { return "None" }
 func (o *PNone) Repr() string           { return "None" }
