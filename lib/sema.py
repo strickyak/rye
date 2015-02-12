4e934c6e98d92230ce1796ef95d05190cee10cc2
strick <strick@localhost>
2015-02-12 00:24:50 -0800

    Shared var & Serial generator, in lib/sema

---
+++ lib/sema.py
@@ -0,0 +1,31 @@
+"""Data Structures safe for use across goroutines."""
+from go import sync
+
+class Shared:
+  "A shared variable, you can Get() and Set(x)."
+  def __init__(x=None):
+    .x = x
+    .mu = go_new(sync.Mutex)
+  def Get():
+    .mu.Lock()
+    z = .x
+    .mu.Unlock()
+    return z
+  def Set(x):
+    .mu.Lock()
+    .x = x
+    .mu.Unlock()
+
+class Serial:
+  "A serial number generator, starting at i=1."
+  def __init__(i=1):
+    .i = i
+    .mu = go_new(sync.Mutex)
+  def Next():
+    .mu.Lock()
+    z = .i
+    .i += 1
+    .mu.Unlock()
+    return z
+
+pass
