0d8ff1a3df1bc93b6af618db686ffbdf932f0ab6
strick <strick@yak.net>
2016-01-24 20:50:16 -0800

    docs & typs for os.py

--- rye_pye/os.py
+++ rye_pye/os.py
@@ -1,29 +1,33 @@
 from go import os
 from go import path/filepath
 
-def getenv(s):
+def getenv(s : str) -> str:
+  "Get the environment variable, or return empty string if missing."
   return os.Getenv(s)
 
-def makedirs(s, mode=0777):
+def makedirs(s : str, mode=0777) -> None:
+  "Make the dir with the given mode, making dirs as needed along the way."
   os.MkdirAll(s, mode)
 
-def getcwd():
+def getcwd() -> str:
+  "Return the current directory."
   return os.Getwd()
 
 class PathCls:
   def __init__():
     pass
-  def basename(x):
+  def basename(x : str) -> str:
     return filepath.Base(x)
-  def dirname(x):
+  def dirname(x : str) -> str:
     return filepath.Dir(x)
-  def join(*args):
+  def join(*args) -> str:
     return filepath.Join(*args)
 
 path = PathCls()
 
 class stat:
-  def __init__(filename):
+  "For the given filename, return an object with .st_size & .st_mtime fields."
+  def __init__(filename : str):
     fi = os.Stat(filename)
     .st_size = fi.Size()
     .st_mtime = fi.ModTime().Unix()
