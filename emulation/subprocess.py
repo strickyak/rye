6cd99df756c04df02288ff47e30c2531d1273224
strick <strick@yak.net>
2018-10-22 16:35:23 -0700

    emulation/subprocess::call panic on exception.

--- emulation/subprocess.py
+++ emulation/subprocess.py
@@ -1,16 +1,15 @@
+from go import log
 from go import os
 from go import os/exec as E
 
-def call(vec, stdin=None, stdout=None, stderr=None):
+def call(vec :list, stdin=None, stdout=None, stderr=None) ->int :
   try:
     cmd = E.Command(vec[0], *vec[1:])
-    # See PYE_FileDesc in builtins.py, for why .f:
+    # See RyeFileDesc in builtins.py, for why .f:
     cmd.Stdin = os.Stdin if stdin is None else stdin.f
     cmd.Stdout = os.Stdout if stdout is None else stdout.f
     cmd.Stderr = os.Stderr if stderr is None else stderr.f
     cmd.Run()
     return 0
   except as ex:
-    msg = 'PYE: subcommand.call: ERROR <<<%s>>> CALLING <<<%s>>>' % (ex, repr(vec))
-    say msg
-    return 13
+    log.Panicf('emulation/subcommand.call: ERROR %q WHILE CALLING %v', ex, vec)
